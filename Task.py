89954ac943b1e69a03a787c9110819d42371bfe7
Your Name <you@example.com>
2021-03-21 10:50:50 +0300

    first commit

---
+++ Task.py
@@ -0,0 +1,59 @@
+#Времена года
+def season(mounth):
+    if (mounth == 12):
+        a = "Зима"
+        return a
+    if (mounth == 1):
+        b = "Зима"
+        return b
+    if (mounth == 2):
+        c = "Зима"
+        return c
+    if (mounth == 3):
+        d = "Весна"
+        return d
+    if (mounth == 4):
+        i = "Весна"
+        return i
+    if (mounth == 5):
+        n = "Весна"
+        return n
+    if (mounth == 6):
+        g = "Лето"
+        return g
+    if (mounth == 7):
+        e = "Лето"
+        return e
+    if (mounth == 8):
+        f = "Лето"
+        return f
+    if (mounth == 9):
+        h = "Осень"
+        return h
+    if (mounth == 10):
+        y = "Осень"
+        return y
+    if (mounth == 11):
+        x = "Осень"
+        return x
+    if (mounth == 12):
+        k = "Зима"
+        return k
+    else:
+        q="Incorrect number of mounth"
+        return q
+# print(season(6))
+
+
+#банковский вклад
+def bank(a,years):
+    g = (a+((a/100*10)*(12*years)))
+    return g
+# print(bank(1000,2))
+# Простое число
+def is_prime(Number):
+    if and :
+        return True
+    else:
+        return  False
+print(is_prime(4))
