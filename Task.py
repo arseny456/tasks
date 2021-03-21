bf0bfbed2332b48bb2843a523e780af61d056636
Your Name <you@example.com>
2021-03-21 14:29:42 +0300

    second commit

--- Task.py
+++ Task.py
@@ -1,66 +1,73 @@
 #Времена года
-def season(mounth):
-    if (mounth == 12):
-        a = "Зима"
-        return a
-    if (mounth == 1):
-        b = "Зима"
-        return b
-    if (mounth == 2):
-        c = "Зима"
-        return c
-    if (mounth == 3):
-        d = "Весна"
-        return d
-    if (mounth == 4):
-        i = "Весна"
-        return i
-    if (mounth == 5):
-        n = "Весна"
-        return n
-    if (mounth == 6):
-        g = "Лето"
-        return g
-    if (mounth == 7):
-        e = "Лето"
-        return e
-    if (mounth == 8):
-        f = "Лето"
-        return f
-    if (mounth == 9):
-        h = "Осень"
-        return h
-    if (mounth == 10):
-        y = "Осень"
-        return y
-    if (mounth == 11):
-        x = "Осень"
-        return x
-    if (mounth == 12):
-        k = "Зима"
-        return k
-    else:
-        q="Incorrect number of mounth"
-        return q
-# print(season(6))
+# def season(mounth):
+#     if (mounth == 12):
+#         a = "Зима"
+#         return a
+#     if (mounth == 1):
+#         b = "Зима"
+#         return b
+#     if (mounth == 2):
+#         c = "Зима"
+#         return c
+#     if (mounth == 3):
+#         d = "Весна"
+#         return d
+#     if (mounth == 4):
+#         i = "Весна"
+#         return i
+#     if (mounth == 5):
+#         n = "Весна"
+#         return n
+#     if (mounth == 6):
+#         g = "Лето"
+#         return g
+#     if (mounth == 7):
+#         e = "Лето"
+#         return e
+#     if (mounth == 8):
+#         f = "Лето"
+#         return f
+#     if (mounth == 9):
+#         h = "Осень"
+#         return h
+#     if (mounth == 10):
+#         y = "Осень"
+#         return y
+#     if (mounth == 11):
+#         x = "Осень"
+#         return x
+#     if (mounth == 12):
+#         k = "Зима"
+#         return k
+#     else:
+#         q="Incorrect number of mounth"
+#         return q
+# # print(season(6))
 
 
 #банковский вклад
-def bank(a,years):
-   result = 0
-   for i in range(0, years):
-       print(a)
-       a = a * 1.1
-       result = a
-       return result
+# def bank(a,years):
+#    result = 0
+#    for i in range(0, years):
+#        print(a)
+#        a = a * 1.1
+#        result = a
+#        return result
 ##print(bank(1000,15))
 # Простое число
-def is_prime(Number):
-        for b in range(0,Number+1):
-            if b == 1 or b == Number or b==0 :
-                continue
-            if Number % b == 0:
-                return False
-            else:
-                return  True
-print(is_prime(2))
+# def is_prime(Number):
+#         for b in range(0,Number+1):
+#             if b == 1 or b == Number or b==0 :
+#                 continue
+#             if Number % b == 0:
+#                 return False
+#             else:
+#                 return  True
+# print(is_prime(2))
+#Правильная дата
+# def date(date,mounth,year):
+#     if date <= 31 or mounth <= 12 or year <=2021:
+#         return
+#     else:
+#         return
+# print(date(31,12,2121))
